3900157c2fc7f011b601b266db9a51c14922e96a
AndreyMustafin2001 <andrey.must2001@gmail.com>
2023-12-21 00:31:04 +0400

    group pairs
    Сортируется вектор пар по координате y

--- lines.cpp
+++ lines.cpp
@@ -1,6 +1,32 @@
+#include <iostream>
 #include <vector>
-
-int line(std::vector<pair> Dots)
+#include <map>
+void readAllGroups(const std::map<double, std::vector<std::pair<double, double>>>& groupPairs)
 {
+    auto it = groupPairs.begin();
+    for (it = groupPairs.begin(); it != groupPairs.end(); ++it)
+    {
+        std::cout << "Group y=" << it->first << ":" << std::endl;
 
+        for (const auto& pair : it->second)
+        {
+            std::cout << "(" << pair.first << ", " << pair.second << ")" << std::endl;
+        }
+    }
 }
+int main()
+{
+    std::vector<std::pair<double, double>> pairs = {{1, 2}, {3, 2}, {5, 2}, {3, 6},{1,9}};
+    // Using std::map to automatically sort by y coordinate
+    std::map<double, std::vector<std::pair<double, double>>> groupPairs;
+
+    for (const auto& pair : pairs)
+    {
+        groupPairs[pair.second].push_back(pair);
+    }
+
+    readAllGroups(groupPairs);
+
+    return 0;
+}
+

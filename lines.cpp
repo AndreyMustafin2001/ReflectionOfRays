dcff898910ef89a20e3e286ccce205fd6a6b7324
AndreyMustafin2001 <andrey.must2001@gmail.com>
2023-12-21 01:01:02 +0400

    isSymmetric
    Добавлена функция  isSymmetric ,которая по данной прямой и точкам проверяет, существуют ли симметричные точки в группе.

--- lines.cpp
+++ lines.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <map>
+
+bool isSymmetric(const std::vector<std::pair<double, double>>& pointGroup, double middleX)
+{
+    double distanceToLeft = 0.0;
+    double distanceToRight = 0.0;
+
+    std::map<double,double> have;
+    for (const auto& pair : pointGroup)
+    {
+        have[pair.first]=1;
+    }
+
+    for (const auto& pair : pointGroup)
+    {
+        double symmx;
+        if (pair.first <= middleX)
+        {
+            distanceToLeft = middleX - pair.first;
+            symmx = middleX + distanceToLeft;
+        }
+        else if (pair.first > middleX)
+        {
+            distanceToRight = pair.first - middleX;
+            symmx = middleX - distanceToRight;
+        }
+        if(have.count(symmx) == 0)
+            return false;
+
+    }
+    return true;
+}
+
 void readAllGroups(const std::map<double, std::vector<std::pair<double, double>>>& groupPairs)
 {
-
+    bool answer = true;
     auto it = groupPairs.begin();
     double minX = it->second.begin()->first;
     for (const auto& p : it->second)
@@ -23,19 +55,26 @@ void readAllGroups(const std::map<double, std::vector<std::pair<double, double>>
     std::cout << "MiddleX=" << middleX << std::endl;
 
     for (it = groupPairs.begin(); it != groupPairs.end(); ++it)
-    {
-        std::cout << "Group y=" << it->first << ":" << std::endl;
+        {
 
+        std::cout << "Group y=" << it->first << ":" << std::endl;
         for (const auto& pair : it->second)
         {
             std::cout << "(" << pair.first << ", " << pair.second << ")" << std::endl;
         }
-    }
+
+            bool symmetricResult = isSymmetric(it->second, middleX);
+            if(symmetricResult == false)answer = false;
+            std::cout << "Simmetric ?: " << (symmetricResult ? "TRUE" : "FALSE") << std::endl;
+        }
+    if(answer == true){std::cout << "x="<< middleX << std::endl << "true";}
+    else std::cout << "false";
 }
+
 int main()
 {
-    std::vector<std::pair<double, double>> pairs = {{1, 2}, {3, 2}, {5, 2}, {3, 6},{1,9}};
-    // Using std::map to automatically sort by y coordinate
+    std::vector<std::pair<double, double>> pairs = {{1, 2}, {3, 2}, {5, 2}, {3, 6}};
+
     std::map<double, std::vector<std::pair<double, double>>> groupPairs;
 
     for (const auto& pair : pairs)
